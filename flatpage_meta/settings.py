51960a18fb8d86ecb48eefd75ba9f4e370b3878e
Jirka Schaefer <jirka@tschitschereengreen.com>
2024-12-18 10:17:29 +0100

    add default test database

--- flatpage_meta/settings.py
+++ flatpage_meta/settings.py
@@ -1,1 +1,5 @@
 INSTALLED_APPS = ["flatpage_meta", "django.contrib.sites", "django.contrib.flatpages"]
+
+DATABASES = {
+    "default": {"ENGINE": "sqlite3", "NAME": "", "USER": "", "PASSWORD": ""},
+}
